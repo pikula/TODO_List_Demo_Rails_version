e45c982c5996da7c8d5f6ad882a21e139c8800ad
Pikula <azra241@yahoo.com>
2012-11-02 22:53:07 +0100

    minor fixes in organization

--- todolist/todolist/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Todolist::Application.config.secret_token = '73171c8eecb3c59354a867bccfb5c0403b27a7050820638815a3d361af3a9c57680f71b55179dd1c9aaa843667ebe123802b615ad6c7888e83fcc1fff40ec122'
--- todolist/todolist/test/unit/helpers/todoitem_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TodoitemHelperTest < ActionView::TestCase
-end
